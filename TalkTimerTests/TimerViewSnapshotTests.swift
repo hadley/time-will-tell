814f86927c01df93919d371c2f5a4c4ead5dab09
Hadley Wickham <h.wickham@gmail.com>
2026-01-11 08:36:40 -0600

    Add some snapshot tests

---
+++ TalkTimerTests/TimerViewSnapshotTests.swift
@@ -0,0 +1,55 @@
+import SnapshotTesting
+import SwiftUI
+@testable import TalkTimer
+import Testing
+
+/// Test wrapper that displays a timer with specific text
+private struct TimerSnapshotView: View {
+    let displayText: String
+
+    var body: some View {
+        ZStack {
+            Color.black.ignoresSafeArea()
+            ScalableTimerText(text: displayText, textColor: .white)
+        }
+    }
+}
+
+@MainActor
+struct TimerViewSnapshotTests {
+    @Test func timerDisplaySizing() {
+        let view = TimerView()
+
+        // iPhone landscape
+        assertSnapshot(
+            of: view,
+            as: .image(layout: .device(config: .iPhone13ProMax(.landscape))),
+            named: "iPhone"
+        )
+
+        // iPad landscape
+        assertSnapshot(
+            of: view,
+            as: .image(layout: .device(config: .iPadPro12_9(.landscape))),
+            named: "iPad"
+        )
+    }
+
+    @Test func narrowTimerDisplay() {
+        let view = TimerSnapshotView(displayText: "01:11")
+
+        // iPhone landscape
+        assertSnapshot(
+            of: view,
+            as: .image(layout: .device(config: .iPhone13ProMax(.landscape))),
+            named: "iPhone"
+        )
+
+        // iPad landscape
+        assertSnapshot(
+            of: view,
+            as: .image(layout: .device(config: .iPadPro12_9(.landscape))),
+            named: "iPad"
+        )
+    }
+}
